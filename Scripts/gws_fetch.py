34352c283223cbadecf72aabdae5aa0547b6460e
lipastomies <arto.lehisto@gmail.com>
2019-04-09 14:49:36 +0300

    cleaned script

--- Scripts/gws_fetch.py
+++ Scripts/gws_fetch.py
@@ -3,15 +3,15 @@
 import argparse,shlex,subprocess
 import pandas as pd 
 import numpy as np
-#read dataframe, it's basically a tab delimited csv
 
 def fetch_gws(args):
     fname=args.fpath
     sig_tresh=args.sig_treshold
-    #get data
+
     c_size=100000
     result_dframe=None
     r=args.loc_width#range for location width
+    #get data
     for dframe in pd.read_csv(fname,compression="gzip",sep="\t",dtype={"#chrom":str,
         "pos":np.int32,
         "ref":str,
@@ -41,13 +41,6 @@ def fetch_gws(args):
     print("filtered gws values")
     new_df=None
     if args.grouping:
-        #PROBLEM
-        #what if we search overlapping areas? setting the group will set them erratically, and give wrong results
-        #if we think of the tabix-fetched rows as one list of rows (worse, we will have duplicate rows, and how do we dowrk with those?
-        # filter them out?) Oh well, most likely we won't come across this in the beginning, as the rows are filtered based on the secondary
-        #pval, but it might happen.
-        #First, make tabix calls and write them to a file, then fetch all the relevant rows
-        #so, we need the chromosome and the min and max locations
         tcall="tabix "+fname+" -h "
         for _idx,row in result_dframe.iterrows():
             tcall =tcall+" chr "+row["#chrom"]+":"+str(row["pos_rmin"])+"-"+str(row["pos_rmax"])
@@ -56,41 +49,29 @@ def fetch_gws(args):
         #execute tabix call
         with open("temp.out","w") as out: 
             tbx=subprocess.run(call,stdout=out)
-        #load ouput file
-        print("Tabix ran")
+        #print("Tabix ran")
         tabixdf=pd.read_csv("temp.out",sep="\t",dtype={"#chrom":str,
-        "pos":np.int32,
-        "ref":str,
-        "alt":str,
-        "rsids":str,
-        "nearest_genes":str,
-        "pval":np.float64,
-        "beta":np.float64,
-        "sebeta":np.float64,
-        "maf":np.float64,
-        "maf_cases":np.float64,
-        "maf_controls":np.float64
-        },engine="c")
-
-        #then, we need to 
-        # 1)filter them according to p-value
+                                                        "pos":np.int32,
+                                                        "ref":str,
+                                                        "alt":str,
+                                                        "rsids":str,
+                                                        "nearest_genes":str,
+                                                        "pval":np.float64,
+                                                        "beta":np.float64,
+                                                        "sebeta":np.float64,
+                                                        "maf":np.float64,
+                                                        "maf_cases":np.float64,
+                                                        "maf_controls":np.float64
+                                                        },engine="c")
         tabixdf=tabixdf[tabixdf["pval"]<args.sig_treshold_2]
-        #remove duplicates from tabixdf
-        print("drop duplicates")
+        #print("drop duplicates")
         tabixdf=tabixdf.drop_duplicates(subset=["#chrom","pos"])
-        # 2) attach the correct groups, I guess
-        #this might be harder to do.
         new_df=pd.DataFrame(columns=result_dframe.columns).drop(["pos_rmin","pos_rmax"],axis=1)
         for t in result_dframe.itertuples():
             rowidx=(tabixdf["pos"]<=t.pos_rmax)&(tabixdf["pos"]>=t.pos_rmin)
             tmp=tabixdf.loc[rowidx,:].copy()
             tmp.loc[:,"locid"]=t.locid
-            #print(tmp)
-            new_df=pd.concat([new_df,tmp],ignore_index=True,axis=0,join='inner',copy=False)
-            #tabixdf.loc[rowidx,"locid"]=t.locid
-        # it makes no sense to call tabix many times just because it would be easier to attach a group id that way
-        #instead, we might iterate over the resulting rows? It might take time but shouldn't be too slow, since the 
-        #dataframe isn't that big anymore (maybe a few million rows max)
+            new_df=pd.concat([new_df,tmp],ignore_index=True,axis=0,join='inner')
     if args.grouping:
         new_df.to_csv(path_or_buf=args.out_fname,sep="\t",index=False)
     else:
