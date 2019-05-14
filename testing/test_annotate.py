7967f406be7894c11938854e87c6871d0eb6156f
Lehisto Arto <arto.lehisto@helsinki.fi>
2019-05-14 14:25:40 +0300

    added some test cases for annotation, changed folder structure of testing

---
+++ testing/test_annotate.py
@@ -0,0 +1,75 @@
+import unittest
+import sys,os,io
+sys.path.append("../")
+sys.path.append("./")
+import pandas as pd,numpy as np
+from Scripts import annotate
+
+class Arg():
+    def __init__(self):
+        pass
+
+class TestAnnotate(unittest.TestCase):
+    def test_rename_dict(self):
+        lst=["A","B","C"]
+        result={"A":"Prefix_A","B":"Prefix_B","C":"Prefix_C"}
+        d=annotate.create_rename_dict(lst,"Prefix_")
+        self.assertEqual(d,result)
+        lst=[]
+        result={}
+        d=annotate.create_rename_dict(lst,"Prefix_")
+        self.assertEqual(d,result)
+
+    def test_calculate_enrichment(self):
+        #load "gnomad" df
+        g_df=pd.read_csv("annotate_resources/enrichment_df",sep="\t")
+        g_df=g_df.apply(pd.to_numeric,errors="ignore")
+        fi_af_col="AF_fin"
+        count_nfe_lst=["AC_1","AC_2","AC_3"]
+        number_nfe_lst=["AN_1","AN_2","AN_3"]
+        result=pd.Series([2.0,1e6,0.0])
+        test=annotate.calculate_enrichment(g_df,fi_af_col,count_nfe_lst,number_nfe_lst)
+        for idx,_val in result.iteritems():
+            self.assertAlmostEqual(result[idx],test[idx]) 
+
+    def test_annotate(self):
+        #define args
+        #needed files: gnomad_genome_tabixed file, exome gnomad, finngen annotations
+        #also, gws file
+        #test so that we either have 0 or more lines matching to the annotation files,
+        #to test if they work with zero or more lines
+        #best to make a df with lines 1-2 corresponding to genomes df, 3-4 to exomes, 
+        #and 4-6 to finngen annotations
+        #then, test it, choosing only some lines for the df file
+        #this might be best to do with stringio, loading the relevant lines and passing that as the path argument 
+        #for annotate
+        args=Arg()
+        args.gnomad_genome_path="annotate_resources/gnomad_genomes.tsv.gz"
+        args.gnomad_exome_path="annotate_resources/gnomad_exomes.tsv.gz"
+        args.finngen_path="annotate_resources/finngen_anno.tsv.gz"
+        args.out_fname="annotate_resources/test_out.csv"
+        args.batch_freq=True
+        try:
+            with open("annotate_resources/annotate_df.tsv","r") as f:
+                #test case lines
+                lines=f.readlines()
+                test_cases=[[0,1,2,3,4],[0,3,4,5,6],[0,1,2,5,6],[0,1,2,3,4,5,6]]
+                for tcase in test_cases:
+                    #load the lines that appear in test case
+                    test_lines=[lines[x] for x in tcase]
+                    tmp="".join(test_lines)
+                    with io.StringIO(tmp) as args.annotate_fpath:
+                        annotate.annotate(args)
+                        with open(args.out_fname,"r") as f:
+                            df=pd.read_csv(f,sep="\t")
+                            #TODO: check whether the file is correct
+        except:
+            self.assertTrue(False)
+
+
+
+        
+
+if __name__=="__main__":
+    os.chdir("./testing")
+    unittest.main()
--- testing/test_fetch.py
+++ testing/test_fetch.py
@@ -1,5 +1,5 @@
 import unittest
-import sys
+import sys,os
 sys.path.append("../")
 sys.path.append("./")
 import pandas as pd,numpy as np
@@ -61,7 +61,7 @@ class TestGws(unittest.TestCase):
 
     def test_plink_parsing(self):
         #load data
-        df=pd.read_csv("testing/test_plink.clumped",sep="\s+")
+        df=pd.read_csv("fetch_resources/test_plink.clumped",sep="\s+")
         df=df.loc[:,["SNP","TOTAL","SP2"]]
         #create end result
         df2={"#chrom":["1","1","X","X","X"],
@@ -78,11 +78,11 @@ class TestGws(unittest.TestCase):
         #create parameters for function
         result_header=["#chrom", "pos", "ref", "alt", "rsids", "nearest_genes", "pval", "beta", "sebeta", "maf", "maf_cases", "maf_controls","#variant","locus_id"]
         df=pd.DataFrame(columns=result_header)
-        group_df=pd.read_csv("testing/test_plink.clumped",sep="\s+")
+        group_df=pd.read_csv("fetch_resources/test_plink.clumped",sep="\s+")
         group_df=group_df.loc[:,["SNP","TOTAL","SP2"]]
-        tabixdf=pd.read_csv("testing/test_tsv",sep="\t")
+        tabixdf=pd.read_csv("fetch_resources/test_tsv",sep="\t")
         tabixdf.loc[:,"#variant"]=gws_fetch.create_variant_column(tabixdf)
-        df2=pd.read_csv("testing/solve_groups_result.tsv",sep="\t")
+        df2=pd.read_csv("fetch_resources/solve_groups_result.tsv",sep="\t")
         df=gws_fetch.solve_groups(df,group_df,tabixdf)
         df.loc[:,"pos"]=df.loc[:,"pos"].astype(np.int64)
         #test for equality
@@ -91,4 +91,5 @@ class TestGws(unittest.TestCase):
 
 
 if __name__=="__main__":
+    os.chdir("./testing")
     unittest.main()
