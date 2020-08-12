943cb085b4ba345603b94f9e2574d4e084cdaa2d
Lipastomies <arto.lehisto@gmail.com>
2020-08-12 15:53:02 +0300

    remove obsolete test_annotate.test_annotate

--- testing/test_annotate.py
+++ testing/test_annotate.py
@@ -33,48 +33,7 @@ class TestAnnotate(unittest.TestCase):
         test=annotate.calculate_enrichment(g_df,fi_af_col,count_nfe_lst,number_nfe_lst)
         for idx,_val in result.iteritems():
             self.assertAlmostEqual(result[idx],test[idx]) 
-    """
-    def test_annotate(self):
-        #define args
-        #needed files: gnomad_genome_tabixed file, exome gnomad, finngen annotations
-        #also, gws file
-        #test so that we either have 0 or more lines matching to the annotation files,
-        #to test if they work with zero or more lines
-        #best to make a df with lines 1-2 corresponding to genomes df, 3-4 to exomes, 
-        #and 4-6 to finngen annotations
-        #then, test it, choosing only some lines for the df file
-        #this might be best to do with stringio, loading the relevant lines and passing that as the path argument 
-        #for annotate
-        args=Arg()
-        args.gnomad_genome_path="testing/annotate_resources/gnomad_genomes.tsv.gz"
-        args.gnomad_exome_path="testing/annotate_resources/gnomad_exomes.tsv.gz"
-        args.finngen_path="testing/annotate_resources/finngen_anno.tsv.gz"
-        args.annotate_out="testing/annotate_resources/test_out.csv"
-        args.functional_path=None
-        args.previous_release_path=None
-        args.prefix=""
-        correct_value_path="testing/annotate_resources/ann_validate"
-        args.batch_freq=False
-        columns={"chrom":"#chrom", "pos":"pos", "ref":"ref", "alt":"alt", "pval":"pval", "beta":"beta", "af":"af"}
-        try:
-            with open("testing/annotate_resources/annotate_df.tsv","r") as f:
-                #test case lines
-                lines=f.readlines()
-                test_cases=[[0,1,2,3,4],[0,3,4,5,6],[0,1,2,5,6],[0,1,2,3,4,5,6]]
-                for i, tcase in enumerate(test_cases):
-                    #load the lines that appear in test case
-                    test_lines=[lines[x] for x in tcase]
-                    tmp="".join(test_lines)
-                    with io.StringIO(tmp) as args.annotate_fpath:
-                        in_df = pd.read_csv(args.annotate_fpath,sep="\t")
-                        out = annotate.annotate(df=in_df,gnomad_genome_path=args.gnomad_genome_path, gnomad_exome_path=args.gnomad_exome_path, batch_freq=args.batch_freq, finngen_path=args.finngen_path,
-                            functional_path=args.functional_path, previous_release_path=args.previous_release_path, prefix=args.prefix, columns=columns).astype(object)
-                        df2=pd.read_csv("{}_{}.csv".format(correct_value_path,i+1),sep="\t").astype(object)
-                        for col in df2.columns:
-                            pd.testing.assert_series_equal(out[col],df2[col])
-        except:
-            self.assertTrue(False)
-    """
+    
     def test_func_anno(self):
         """Test functional annotation
         Cases:
